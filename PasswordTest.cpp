9c1418de958676365204529a61ae3c4ee85df1ff
copper-glitch <142946597+copper-glitch@users.noreply.github.com>
2025-04-23 20:45:58 -0700

    Update PasswordTest.cpp

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,35 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+TEST(PasswordTest, two_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZZ");
+	ASSERT_EQ(2, actual);
+}
+TEST(PasswordTest, Two_letter_different_case_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("Zz");
+	ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, four_letter_sep_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aZaa");
+	ASSERT_EQ(1, actual);
+	int actual2 = my_password.count_leading_characters("aaza");
+	ASSERT_EQ(2, actual2);
+}
+TEST(PasswordTest, long_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+	ASSERT_EQ(57, actual);
+}
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
