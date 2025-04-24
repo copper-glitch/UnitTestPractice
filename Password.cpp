f802f4e95367f0687977b57ef14c3f49aa45bec2
copper-glitch <142946597+copper-glitch@users.noreply.github.com>
2025-04-23 20:45:30 -0700

    Update Password.cpp

--- Password.cpp
+++ Password.cpp
@@ -10,13 +10,17 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
-  int repetition = 1;
-  int index = 0;
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
-    repetition++;
-    index++;
+  if (phrase!="")
+  {
+    int repetition = 1;
+    int index = 0;
+    while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
+      repetition++;
+      index++;
+    }
+    return repetition;
   }
-  return repetition;
+  return 0;
 }
 
 
